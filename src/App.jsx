392fca9b65bd3e61a593289ca6fae6f20b01173d
hudsonr <rose.hudson@lightsource.ca>
2024-11-21 12:15:16 -0600

    Created basic version of birthday input fields.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,41 @@ import { useState } from 'react'
 import './App.css'
 
 import Header from './components/Header'
+import InputSection from './components/InputSection';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [birthday, setBirthday] = useState({Day: 1, Month: 1, Year: 2000});
+  const [units, setUnits] = useState(['Seconds', 'Hours', 'Days', 'Weeks', 'Months', 'Years', 'Decades'])
+
+  function handleBirthdayChange(area, newVal) {
+    setBirthday((prevBirthday) => {
+      return {
+        ...prevBirthday,
+        [area]: newVal,
+      };
+    });
+  }
 
   return (
     <>
     <div>
       <Header />
-      <h2> inputs x3 </h2>
-      <h2> units </h2>
+      <InputSection 
+        area='Day'
+        prevVal={birthday['Day']}
+        onValChange={handleBirthdayChange}
+      />
+      <InputSection 
+        area='Month'
+        prevVal={birthday['Month']}
+        onValChange={handleBirthdayChange}
+      />
+      <InputSection 
+        area='Year'
+        prevVal={birthday['Year']}
+        onValChange={handleBirthdayChange}
+      />
+      <h2> unit selection </h2>
       <h2> button </h2>
     </div>
     <div>
--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -1,3 +1,26 @@
-export default function InputSection(){
-    return
+import { useState } from 'react'
+
+export default function InputSection({area, prevVal, onValChange}){
+    const [isEditing, setIsEditing] = useState(false);
+    const [value, setValue] = useState(prevVal);
+
+    function handleEditing() {
+        setIsEditing((editing) => !editing);
+        if (isEditing){
+            onValChange(area, value);
+        }
+    }
+
+    function handleChange(event) {
+        setValue(event.target.value);
+    }
+
+    return(
+        <li>
+        <span>
+            <p>{area}:</p>
+            <input type="number" required value={value} onChange={handleChange} />
+        </span>
+        </li>
+    )
 }
