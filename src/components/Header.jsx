4df1eb04cc7eb15f81b9d5fa4bc8c7bbeb18df2e
hudsonr <rose.hudson@lightsource.ca>
2024-11-21 11:52:13 -0600

    Created initial files and idea of layout in app.jsx.

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return(
+        <div>
+            <h1>How Old Are You?</h1>
+            <p>Fill in the information below, and choose what unit you'd like to see
+                your age in (seconds, hours, days, etc.) from the drop down button. Then,
+                just click the 'Calculate' button and find out!
+            </p>
+        </div>
+    )
+}
